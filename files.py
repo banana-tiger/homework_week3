627761a114cd3ef17c18afdd439114c6509b0cb2
Anton Maliutin <amalutin13@gmail.com>
2020-02-25 00:21:57 +0300

    Добавлено задание по работе с файлами

--- files.py
+++ files.py
@@ -17,7 +17,23 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', "r", encoding="utf-8") as file:
+        referat = ''
+        for line in file:
+            referat += line
+    referat_len = len(referat)
+    trash_list = ['.', ',', '!', '?']
+    words_count = 0
+    for word in referat.split():
+        if word in trash_list:
+            continue
+        else:
+            words_count += 1
+    referat2 = referat.replace('.', '!')
+
+    with open('referat2.txt', 'w', encoding='utf-8') as file:
+        file.write(f'Результаты: длина файла = {referat_len},  количество слов = {words_count}. Измененный текст: '
+                   f'{referat2}')
 
 if __name__ == "__main__":
     main()
